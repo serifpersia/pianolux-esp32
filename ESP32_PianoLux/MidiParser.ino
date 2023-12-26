1e0e5b720fb8ef0ce90628cc28c9a2dcd5ae1642
serifpersia <ramiserifpersia@gmail.com>
2023-12-26 13:20:21 +0100

    add support for sending network midi out

--- ESP32_PianoLux/MidiParser.ino
+++ ESP32_PianoLux/MidiParser.ino
@@ -24,12 +24,20 @@ static void midi_transfer_cb(usb_transfer_t *transfer) {
         // Execute noteOn or noteOff based on the MIDI statusByte
         if (statusByte >= 0x80 && statusByte < 0x90) {
           noteOff(note, velocity);
+          if (isConnected) {
+            MIDI.sendNoteOff(note, velocity, 1);
+          }
         } else if (statusByte >= 0x90 && statusByte < 0xA0) {
           if (velocity == 0) {
-            noteOff(note, velocity); // Treat "Note On" with 0 velocity as "Note Off"
-          }
-          else {
+            noteOff(note, velocity);  // Treat "Note On" with 0 velocity as "Note Off"
+            if (isConnected) {
+              MIDI.sendNoteOff(note, velocity, 1);
+            }
+          } else {
             noteOn(note, velocity);
+            if (isConnected) {
+              MIDI.sendNoteOn(note, velocity, 1);
+            }
           }
         }
       }
